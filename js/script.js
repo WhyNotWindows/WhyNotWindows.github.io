70d2dc4a23d4db4b1dcec29b744e85b43c63cb91
WhyNotWindows <73014184+WhyNotWindows@users.noreply.github.com>
2022-02-24 16:05:16 +0200

    adaptation (-examples)

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     $(".examples__arrow").each(function(i){
         $(this).click(function(){
             if ($(this).hasClass("examples__button_active")){
-                $(".examples__example").eq(i).height(160);
+                $(".examples__example").eq(i).height(157);
             }
             else {
                 $(".examples__example").eq(i).height($(".examples__example").eq(i).height() + $(".examples__descr").eq(i).height());
