b9081e3e368c4512e45c10898a5f5c201c4668ab
WhyNotWindows <73014184+WhyNotWindows@users.noreply.github.com>
2022-02-24 19:13:37 +0200

    examples adaptated

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
     $(".examples__arrow").each(function(i){
         $(this).click(function(){
             if ($(this).hasClass("examples__button_active")){
-                $(".examples__example").eq(i).height(157);
+                //$(".examples__example").eq(i).height($(".examples__img").eq(i).height());
+                $(".examples__example").eq(i).height(160);
             }
             else {
                 $(".examples__example").eq(i).height($(".examples__example").eq(i).height() + $(".examples__descr").eq(i).height());
@@ -16,4 +17,16 @@ $(document).ready(function(){
             $(this).toggleClass("examples__button_active");
         });
     });
+
+    function windowSize() {
+        if ($(document).width() <= '470'){
+            $(".examples__example").each(function(i) {
+                $(".examples__example").eq(i).height($(".examples__img").eq(i).height() - 2);
+            });
+            console.log("if");
+        }
+        console.log("function");
+    }
+
+    $(window).on('load resize', windowSize);
 });
