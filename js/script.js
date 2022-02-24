eee1cab81936b4e4a4a93f8003df685d2dd86e67
WhyNotWindows <73014184+WhyNotWindows@users.noreply.github.com>
2022-02-24 23:04:39 +0200

    animations&examples

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
             $(".examples__example").eq(i).toggleClass("examples__example_active");
         });
     }); */
+
+    //example toggle
     $(".examples__arrow").each(function(i){
         $(this).click(function(){
             if ($(this).hasClass("examples__button_active")){
@@ -15,18 +17,36 @@ $(document).ready(function(){
                 $(".examples__example").eq(i).height($(".examples__example").eq(i).height() + $(".examples__descr").eq(i).height());
             }
             $(this).toggleClass("examples__button_active");
+            $(".examples__img").eq(i).toggleClass("examples__img_active");
         });
     });
 
+    //example width
     function windowSize() {
-        if ($(document).width() <= '470'){
+        if ($(document).width() <= '575'){
             $(".examples__example").each(function(i) {
                 $(".examples__example").eq(i).height($(".examples__img").eq(i).height() - 2);
+                $(".examples__example").eq(i).width($(".examples__img").eq(i).width());
             });
-            console.log("if");
         }
-        console.log("function");
+        else {
+            $(".examples__example").width(500);
+        }
     }
+
+    //examples img toggle
+    $(".examples__img").each(function(i){
+        $(this).click(function(){
+            if ($(this).hasClass("examples__img_active")){
+                $(".examples__example").eq(i).height($(".examples__img").eq(i).height() - 2);
+            }
+            else {
+                $(".examples__example").eq(i).height($(".examples__example").eq(i).height() + $(".examples__descr").eq(i).height());
+            }
+            $(this).toggleClass("examples__img_active");
+            $(".examples__button").eq(i).toggleClass("examples__button_active");
+        });
+    });
 
     $(window).on('load resize', windowSize);
 });
