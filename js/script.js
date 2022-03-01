3565b4beb8e295ffeb16429fec12785c3554fbe4
WhyNotWindows <73014184+WhyNotWindows@users.noreply.github.com>
2022-03-01 18:40:06 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu__link'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
 $(document).ready(function(){
 /*     $(".examples__arrow").each(function(i){
         $(this).click(function(){
@@ -54,4 +72,6 @@ $(document).ready(function(){
     });
 
     $(window).on('load resize', windowSize);
+
+    new WOW().init();
 });
